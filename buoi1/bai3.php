f766dfe16266aa5d4e65cebcf47a66a5de5b1c3a
2k4dhn <nguyen1711dhn@gmail.com>
2024-08-28 10:51:00 +0700

    first commit

---
+++ buoi1/bai3.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Phép tính trên hai số</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="box">
+            <h2>PHÉP TÍNH TRÊN HAI SỐ</h2>
+            <form action="pheptinh.php" method="post">
+                <div class="pheptinh">
+                    <input type="radio" id="cong" name="pheptinh" value="cong">
+                    <label for="cong">Cộng</label>
+
+                    <input type="radio" id="tru" name="pheptinh" value="tru">
+                    <label for="tru">Trừ</label>
+
+                    <input type="radio" id="nhan" name="pheptinh" value="nhan">
+                    <label for="nhan">Nhân</label>
+
+                    <input type="radio" id="chia" name="pheptinh" value="chia">
+                    <label for="chia">Chia</label>
+                </div><br>
+
+                <label for="so1">Số thứ nhất:</label><br>
+                <input type="number" id="so1" name="so1"><br>
+                <label for="so2">Số thứ hai (nếu cần):</label><br>
+                <input type="number" id="so2" name="so2"><br><br>
+
+                <input type="submit" value="Tính">
+            </form>
+        </div>
+
+        <div class="box">
+            <h2>KIỂM TRA SỐ</h2>
+            <form action="kiemtra.php" method="post">
+                <div class="kiemtra">
+                    <input type="radio" id="kiemtrachan" name="pheptinh" value="kiemtrachan">
+                    <label for="kiemtrachan">Kiểm tra số chẵn/lẻ</label>
+
+                    <input type="radio" id="kiemtrannt" name="pheptinh" value="kiemtrannt">
+                    <label for="kiemtrannt">Kiểm tra số nguyên tố</label>
+                </div><br>
+
+                <label for="so1">Số cần kiểm tra:</label><br>
+                <input type="number" id="so1" name="so1"><br><br>
+
+                <input type="submit" value="Kiểm tra">
+            </form>
+        </div>
+    </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ 
+
+    <div>
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Bài Tập Của Tôi</title>
+            <link
+              rel="icon"
+              type="image/png"
+              href="backgr/IMG_1224.JPG"
+              alt="Image"
+              width="50px"
+              height="50px"
+            />
+            <link rel="stylesheet" href="index.css" />
+            <script
+              src="https://kit.fontawesome.com/533aad8d01.js"
+              crossorigin="anonymous"
+            ></script>
+            <link
+              href="https://db.onlinewebfonts.com/c/41cdafd19e156a1f74759f9c88b878ff?family=Google+Sans+Text+Regular"
+              rel="stylesheet"
+            />
+          </head>
+          <body>
+            <header class="container">
+              <div class="NoiDung"></div>
+              <button class="icon">
+                <i class="icon-bar fa-solid fa-bars"></i>
+                <ul class="list-ndung">
+                  <li class="text-1">
+                    <span class="home">Home</span>
+                    <i class="icon-angle fa-solid fa-angle-left"></i>
+                    <ul class="list-home-text">
+                      <li class="text-1"><span>Home 1</span></li>
+                      <li class="text-1"><span>Home 2</span></li>
+                      <li class="text-1"><span>Home 3</span></li>
+                    </ul>
+                  </li>
+                  <li class="text-1">
+                    <span>Information</span>
+                    <i class="icon-angle fa-solid fa-angle-left"></i>
+                  </li>
+                  <li class="text-1">
+                    <span>Homework</span>
+                    <i class="icon-angle fa-solid fa-angle-left"></i>
+                  </li>
+                  <li class="text-1">
+                    <span>Contact</span>
+                    <i class="icon-angle fa-solid fa-angle-left"></i>
+                  </li>
+                  <li class="text-1">
+                    <span>About</span>
+                    <i class="icon-angle fa-solid fa-angle-left"></i>
+                  </li>
+                </ul>
+              </button>
+              <div class="container-content-3">
+                <ul class="list-icon">
+                  <li>
+                    <a href="https://www.instagram.com/ngyyen1711_"
+                      ><i class="icon-instagram fa-brands fa-instagram"></i
+                    ></a>
+                  </li>
+                  <li>
+                    <a href="https://www.facebook.com/2k4dhn"
+                      ><i class="icon-facebook fa-brands fa-facebook-f"></i
+                    ></a>
+                  </li>
+                  <li>
+                    <a href="https://mail.google.com/mail/u/0/#inbox"
+                      ><i class="icon-envelope fa-regular fa-envelope"></i
+                    ></a>
+                  </li>
+                </ul>
+              </div>
+            </header>
+            <div class="header">
+            </div>
+            <section class="section-1">
+              <div class="bod">
+                <div class="col5">
+                  <img src="backgr/IMG_1224.JPG" alt="1" />
+                </div>
+                <div class="col5">
+                  <h1>Đặng Hải Nguyên</h1>
+                  <p>MSV: 222001480</p>
+                  <p>LỚP: CNTT D2022 A</p>
+                  <p>Gmail:nguyen1711dhn@gmail.com</p>
+                  <p>Address:Hoài Đức- Hà Nội</p>
+                  <p>Phone:0358351783</p>
+                </div>
+              </div>
+            </section>
+            <div class="containey">
+              <div class="item">
+                <img src="b6/anh1.jpg" alt="Image 1" />
+                <div class="overlay">
+                    <h2>Buổi 1:</h2>
+                    <a href="./buoi1/bai1.php">Bài 1</a>
+                    <a href="./buoi1/bai2.php">Bài 2</a>           
+                    <a href="./buoi1/bai3.php">Bài 3</a>
+                  </div>
+                </div>
+                <div class="item">
+                  <img src="b6/anh2.JPG" alt="Image 2" />
+                  <div class="overlay">
+                    <h2>Buổi 2:</h2>
+                    <a href="./buoi2/bai3.1.php">Bài 3.1</a>
+                    <a href="./buoi2/bai3.2.php">Bài 3.2</a>
+                    <a href="./buoi2/bai3.3.php">Bài 3.3</a>
+                    <a href="./buoi2/bai4.php">Bài 4</a>
+                  </div>
+                </div>
+                <!-- <div class="item">
+                  <img src="b6/anh3.JPG" alt="Image 3" />
+                  <div class="overlay">
+                    <h2>Buổi 3:</h2>
+                    <a href="">Bài 1</a>
+                    <a href="">Bài 2</a>
+                  </div>
+                </div>
+                <div class="item">
+                  <img src="b6/anh5.JPG" alt="Image 4" />
+                  <div class="overlay">
+                    <h2>Buổi 4:</h2>
+                    <a href="">Bài 1</a>
+                    <a href="">Bài 2</a>
+                  </div>
+                </div>
+                <div class="item">
+                  <img src="b6/anh6.jpg" alt="Image 5" />
+                  <div class="overlay">
+                    <h2>Buổi 5:</h2>
+                    <a href="">Bài 1</a>
+                    <a href="">Bài 2</a>
+                  </div>
+                </div>
+                <div class="item">
+                  <img src="b6/anh7.JPG" alt="Image 6" />
+                  <div class="overlay">
+                    <h2>Buổi 6:</h2>
+                    <a href="">Bài 1</a>
+                    <a href="">Bài 2</a>
+                  </div>
+                </div>
+                <div class="item">
+                  <img src="b6/anh2.JPG" alt="Image 7" />
+                  <div class="overlay">
+                    <h2>Buổi 7:</h2>
+                    <a href="">Bài 1</a>
+                    <a href="">Bài 2</a>
+                    
+                  </div>
+                </div> -->
+              </div>
+            </div>
+          </body>
+        </html>
+        
+        
+        
+        
+    </div>
+</body>
+</body>
+</html>
